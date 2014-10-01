19d171bd908ef39ae695dd7b42776c8ce147a04d
Ethan Barhydt <ethanbarhydt@gmail.com>
2014-10-01 18:26:46 -0500

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name                  "Ethan Barhydt"
+    email                 "ethanbarhydt@gmail.com"
+    password              "foobar"
+    password_confirmation "foobar"
+  end
+end
